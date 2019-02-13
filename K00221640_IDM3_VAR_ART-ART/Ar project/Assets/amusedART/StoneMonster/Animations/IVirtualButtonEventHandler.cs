5c1a979a5bb3587a55dc308f20d0edf9c580ad31
k00221640 <dylon.mccabe@gmail.com>
2019-02-13 16:37:28 +0000

    test upload

---
+++ K00221640_IDM3_VAR_ART-ART/Ar project/Assets/amusedART/StoneMonster/Animations/IVirtualButtonEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+    private GameObject vbButtonObject;
+	// Use this for initialization
+	void Start () {
+        vbButtonObject = GameObject.Find("Button");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+
+    }
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ K00221640_IDM3_VAR_ART-ART/Ar project/Assets/amusedART/StoneMonster/Animations/vbScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using Vuforia;
+
+public class vbScript : MonoBehaviour {
+    int health = 0;
+    //private GameObject vbButtonObject;
+    //private GameObject monster;
+    private Animation animate;
+    // Use this for initialization
+    void Start () {
+        animate = GetComponent<Animation>();
+        
+        //CrossPlatformInputManager.GetButton("Attack");
+        //monster = GameObject.Find("StoneMonster");
+        //vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+
+    void Update()
+    {
+        
+        bool Attack = CrossPlatformInputManager.GetButton("Button");
+        if (Attack.Equals(true))
+        {
+            health++;
+            animate.Play("Anim_Attack");
+        }
+        else
+        {
+            //if (health >= 3)
+            //{
+            //    animate.Play("Anim_Death");
+            //    Debug.Log(health + "health");
+            //}
+            //else
+            //{
+                animate.Play("Anim_Idle");
+            
+
+        }
+    }
+    
+    //void FixedUpdate()
+    //{
+    //    bool Attack = CrossPlatformInputManager.GetButton("Button");
+    //    if (Attack.Equals(true))
+    //    {
+    //        anim.Play("Anim_Attack");
+    //    }
+    //}
+}
